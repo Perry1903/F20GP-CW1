f2cad9564a85d34756fb27d226038b4e47c87fdc
Perry <57232543+Perry1903@users.noreply.github.com>
2021-02-23 00:47:32 +0000

    Gun FX and enemy knockdown

--- Assets/MyProject/Scripts/PistolBehaviour.cs
+++ Assets/MyProject/Scripts/PistolBehaviour.cs
@@ -5,16 +5,31 @@ using UnityEngine;
 public class PistolBehaviour : MonoBehaviour
 {
     public float damage = 10f;
-    public float range = 100f;
+    public float fireRate = 0.25f;
+    public float range = 50f;
+    public float hitForce = 100f;
+
 
     public Camera fpsCamera;
+    public ParticleSystem muzzleFlash;
+    private AudioSource gunAudio;
+    private float nextFire;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gunAudio = GetComponent<AudioSource>();
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire1") && Time.time > nextFire)
         {
+            nextFire = Time.time + fireRate;
             Shoot();
+            gunAudio.Play();
+            muzzleFlash.Play();
         }
     }
 
@@ -25,6 +40,21 @@ public class PistolBehaviour : MonoBehaviour
             out hit, range))
         {
             Debug.Log(hit.transform.name);
+
+            Enemy enemy = hit.transform.GetComponent<Enemy>();
+            if (enemy != null)
+            {
+                enemy.TakeDamage(damage);
+            }
+
+            if (hit.rigidbody != null)
+            {
+                if (enemy.health <= 0f)
+                {
+                    hit.rigidbody.AddForce(-hit.normal * hitForce);
+                }
+            }
         }
     }
+
 }
